a4ce4f0f00e684a6a52205597a07d37c5f8ad460
Afam Agbodike <agbodike@gmail.com>
2025-03-17 11:43:37 -0700

    Basic forecast views with dummy data

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -3,5 +3,13 @@ class WeatherController < ApplicationController
   end
 
   def forecast
+    @address = params[:address]
+    @weather_data = {
+      "current_temp" => 78,
+      "temp_min"     => 56,
+      "temp_max"     => 85,
+      "feels_like"   => 79,
+      "humidity"     => 73
+    }
   end
 end

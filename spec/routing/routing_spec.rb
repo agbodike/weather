a55974772c9a9d59883960bc5a5170695d46b1dd
Afam Agbodike <agbodike@gmail.com>
2025-03-20 14:08:17 -0700

    Add routing specs

---
+++ spec/routing/routing_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'Routing', type: :routing do
+  describe 'Assessments' do
+    it { is_expected.to route(:get, 'weather/index').to 'weather#index' }
+    it { is_expected.to route(:get, 'weather/forecast').to 'weather#forecast' }
+  end
+end
